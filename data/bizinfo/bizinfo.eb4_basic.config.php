e9d9c68a7d36fa45cb8ceaf2187341cb05659e0b
hee yong <71780557+drake1122@users.noreply.github.com>
2026-04-23 12:52:27 +0900

    디비추가

---
+++ data/bizinfo/bizinfo.eb4_basic.config.php
@@ -0,0 +1,36 @@
+<?php
+if (!defined('_EYOOM_')) exit;
+$bizinfo = array(
+	"bi_company_name" => "파츠디에스",
+	"bi_company_bizno" => "108-13-43389",
+	"bi_company_ceo" => "CHOI SANG JIN",
+	"bi_company_tel" => "031-983-7471",
+	"bi_company_fax" => "",
+	"bi_company_sellno" => "제2020 경기김포-2320호",
+	"bi_company_bugano" => "",
+	"bi_company_infoman" => "안미진",
+	"bi_company_infomail" => "min48j@hanmail.net",
+	"bi_cs_tel1" => "031-983-7471",
+	"bi_cs_tel2" => "",
+	"bi_cs_fax" => "",
+	"bi_cs_email" => "min48j@hanmail.net",
+	"bi_cs_time" => "-평일: 오전9시~오후6시
+-토요일/일요일/공휴일 휴무
+-점심시간 오후12시~오후1시까지
+-부품 문의는 채팅으로 해주세요
+-고객센터 운영 시간에 순차적으로 답변 드리겠습니다.",
+	"bi_cs_closed" => "-토요일/일요일/공휴일 휴무",
+	"bi_top_logo" => "6abe239b2d72df533819824a23527b63_QEseRkNM_d86ec420cd726b4ea9f23c532e76fbf69c0f187c.png",
+	"bi_bottom_logo" => "6abe239b2d72df533819824a23527b63_cLtNAwPF_2e289d89334af651283f4da4749735da132e1c66.png",
+	"bi_company_zip" => "10021",
+	"bi_company_addr1" => "경기 김포시 월곶면 용강로57번길 31-17",
+	"bi_company_addr2" => "",
+	"bi_company_addr3" => " (고막리)",
+	"bi_company_addr_jibeon" => "R",
+	"bi_top_mobile_logo" => "6abe239b2d72df533819824a23527b63_jQYURbpw_1282c5e6abc814fb2fe6af8737e17a486275a2d2.png",
+	"bi_bottom_mobile_logo" => "6abe239b2d72df533819824a23527b63_tefv4Gpo_7094a76fe7378e9b1578c1e1f9201ff6678dd22b.png",
+	"bi_top_shoplogo" => "6abe239b2d72df533819824a23527b63_j3zA9xI6_57dad4f0adb8fd43764d7bd8d44ba24b3721e93d.png",
+	"bi_bottom_shoplogo" => "6abe239b2d72df533819824a23527b63_X6Ms8kvn_ced87c4a1ff558058a92b2a0061065c4ff2107f8.png",
+	"bi_top_mobile_shoplogo" => "6abe239b2d72df533819824a23527b63_2VZxFBqz_af4f362bb3c124c5f78e8e06bb151ff25d15654a.png",
+	"bi_bottom_mobile_shoplogo" => "6abe239b2d72df533819824a23527b63_Mun3jrKt_965f843143206be5d23eff86a5f0ff9aa95f3b22.png",
+);
---
+++ data/ebslider/eb4_basic/es_master_1776909913.php
@@ -0,0 +1,17 @@
+<?php
+if (!defined('_EYOOM_')) exit;
+$es_master = array(
+	"es_code" => "1776909913",
+	"es_theme" => "eb4_basic",
+	"es_state" => "1",
+	"es_subject" => "인기 제조사 브랜드",
+	"es_skin" => "shop_basic",
+	"es_text" => "인기 제조사 브랜드",
+	"es_ytplay" => "1",
+	"es_ytmauto" => "2",
+	"es_link_cnt" => "1",
+	"es_image_cnt" => "1",
+	"es_link" => "",
+	"es_target" => "_self",
+	"es_image" => "",
+);
